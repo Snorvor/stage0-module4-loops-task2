d41a83773f2ae16f78a78824e0035ce7b4fbd523
Snorvor <bulezar@gmail.com>
2025-08-11 21:10:22 -0300

    print even numbers from zero

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
+        int initialNumber = 0;
+        while (initialNumber % 2 == 0 && initialNumber <= printTillInclusive) {
+            System.out.println(initialNumber);
+            initialNumber++;
+            System.out.println(initialNumber);
+        }
     }
 }

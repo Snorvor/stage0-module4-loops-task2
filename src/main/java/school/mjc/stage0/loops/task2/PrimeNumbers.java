34d6dc28a64d9f4182cf1383a17491dab96810e2
Snorvor <bulezar@gmail.com>
2025-08-12 19:01:11 -0300

    prints from zero to argument value

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int number = 2; // Start checking from 2 (0 and 1 are not primes)
+
+        while (number <= printToInclusive) {
+            boolean isPrime = true;
+            int divisor = 2;
+
+            // Check if 'number' is prime
+            while (divisor * divisor <= number) {
+                if (number % divisor == 0) {
+                    isPrime = false;
+                    break;
+                }
+                divisor++;
+            }
+
+            if (isPrime && number > 1) {
+                System.out.println(number);
+            }
+
+            number++;
+        }
     }
 }

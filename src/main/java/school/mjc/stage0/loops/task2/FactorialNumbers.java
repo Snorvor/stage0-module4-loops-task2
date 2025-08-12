7e9981c7a8fbbea5daf674b35e33571b52e85859
Snorvor <bulezar@gmail.com>
2025-08-12 19:02:48 -0300

    factorial row

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int i = 0;
 
+        while (i <= printToInclusive) {
+            long factorial = 1;
+            int j = 1;
+
+            // Calculate factorial of i
+            while (j <= i) {
+                factorial *= j;
+                j++;
+            }
+
+            System.out.println(factorial);
+            i++;
+        }
     }
 }

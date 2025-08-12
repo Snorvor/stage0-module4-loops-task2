730b69d1410d2735059a592e1bc81e21bd39be76
Snorvor <bulezar@gmail.com>
2025-08-12 19:06:57 -0300

    zero to var argument

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
+        if (chars == null || chars.length == 0) {
+            System.out.println("No characters provided");
+            return;
+        }
 
+        int i = 0;
+        while (i < chars.length) {
+            System.out.print(chars[i]);
+            i++;
+        }
+        System.out.println(); // Add a newline at the end
     }
 }

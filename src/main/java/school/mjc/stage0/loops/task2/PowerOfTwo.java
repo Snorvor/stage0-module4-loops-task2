9a41861be2a8491e396b2737b8a72bcf7dbbc3ba
Snorvor <bulezar@gmail.com>
2025-08-12 19:04:46 -0300

    power of two

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        if (power < 0) {
+            System.out.println("too much power");
+            return;
+        }
 
+        int exponent = 0;
+        while (exponent <= power) {
+            long result = (long) Math.pow(2, exponent);
+            System.out.println(result);
+            exponent++;
+        }
     }
 }
